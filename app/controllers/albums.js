c25bd1d321abefb9d2e4b06b94fad284326c3adf
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-10-30 13:24:15 -0300

    Added next callback to send errors to errors middleware

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -2,18 +2,18 @@ const albumsService = require('../services/albums');
 const albumsSerializer = require('../serializers/albums');
 const albumPhotosSerializer = require('../serializers/album_photos');
 
-const listAlbums = (_, res) =>
+const listAlbums = (_, res, next) =>
   albumsService
     .listAlbums()
     .then(albums => res.send(albumsSerializer(albums)))
-    .catch(error => error);
+    .catch(next);
 
-const listAlbumPhotos = (req, res) => {
+const listAlbumPhotos = (req, res, next) => {
   const albumId = req.params.id;
   return albumsService
     .listAlbumPhotos(albumId)
     .then(albumPhotos => res.send(albumPhotosSerializer(albumPhotos)))
-    .catch(error => error);
+    .catch(next);
 };
 
 module.exports = {
