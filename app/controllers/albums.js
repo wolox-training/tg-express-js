82415f185a8933eb76ae51e7b99b2d820c896d00
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-10-30 13:23:05 -0300

    Added returns according to PR comments

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -2,16 +2,15 @@ const albumsService = require('../services/albums');
 const albumsSerializer = require('../serializers/albums');
 const albumPhotosSerializer = require('../serializers/album_photos');
 
-const listAlbums = (_, res) => {
+const listAlbums = (_, res) =>
   albumsService
     .listAlbums()
     .then(albums => res.send(albumsSerializer(albums)))
     .catch(error => error);
-};
 
 const listAlbumPhotos = (req, res) => {
   const albumId = req.params.id;
-  albumsService
+  return albumsService
     .listAlbumPhotos(albumId)
     .then(albumPhotos => res.send(albumPhotosSerializer(albumPhotos)))
     .catch(error => error);
