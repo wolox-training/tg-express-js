ffd9e7efeb437315d3eb24787d190615e6a49e43
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-10-31 12:01:43 -0300

    Changed serialization to respect best practices

--- app/serializers/album_photos.js
+++ app/serializers/album_photos.js
@@ -1,8 +1,9 @@
-module.exports = albumPhotos =>
-  albumPhotos.map(photo => ({
+module.exports = albumPhotos => ({
+  album_photos: albumPhotos.map(photo => ({
     album_id: photo.albumId,
     id: photo.id,
     title: photo.title,
     url: photo.url,
     thumbnail_url: photo.thumbnailUrl
-  }));
+  }))
+});
--- app/serializers/albums.js
+++ app/serializers/albums.js
@@ -1,6 +1,7 @@
-module.exports = albums =>
-  albums.map(album => ({
+module.exports = albums => ({
+  albums: albums.map(album => ({
     user_id: album.userId,
     id: album.id,
     title: album.title
-  }));
+  }))
+});
