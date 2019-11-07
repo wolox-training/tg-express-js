d8eb8e3d72b93a49bde05f4da4ee0d663b9ad38a
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-07 11:53:26 -0300

    Added api-docs route

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,12 @@
-// const controller = require('./controllers/controller');
+const swaggerUi = require('swagger-ui-express');
 const { healthCheck } = require('./controllers/healthCheck');
 const albumsController = require('./controllers/albums');
 const usersController = require('./controllers/users');
+const documentation = require('../documentation');
 
 exports.init = app => {
   app.get('/health', healthCheck);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(documentation));
 
   app.get('/albums', albumsController.listAlbums);
   app.get('/albums/:id/photos', albumsController.listAlbumPhotos);
