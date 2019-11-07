6c6cd5b88ab5e5f50afdb5b60fdf7861eed1e4d8
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-07 17:39:58 -0300

    Changes to files to use new middleware files

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,9 +1,13 @@
-const signUpInteractor = require('../interactors/sign_up');
+const usersService = require('../services/users');
+
+const userSignUpRequest = require('../serializers/user_sign_up_request');
+const userSignUpResponse = require('../serializers/user_sign_up_response');
 
 const signUp = (req, res, next) => {
   const { user } = req.body;
-  return signUpInteractor(user)
-    .then(createdUser => res.send(createdUser))
+  return usersService
+    .signUp(userSignUpRequest(user))
+    .then(createdUser => res.send(userSignUpResponse(createdUser)))
     .catch(next);
 };
 
--- app/services/users.js
+++ app/services/users.js
@@ -1,21 +1,17 @@
 const models = require('../models/index');
 const logger = require('../logger');
 const errors = require('../errors');
-const { hash } = require('../helpers/utils');
 
 const signUp = user =>
-  hash(user.password, 10).then(hashed_password => {
-    user.password = hashed_password;
-    return models.users
-      .create(user)
-      .then(createdUser => {
-        logger.info(`New user created: ${createdUser.email}`);
-        return createdUser;
-      })
-      .catch(error => {
-        throw errors.databaseError(error.message);
-      });
-  });
+  models.users
+    .create(user)
+    .then(createdUser => {
+      logger.info(`New user created: ${createdUser.email}`);
+      return createdUser;
+    })
+    .catch(error => {
+      throw errors.databaseError(error.message);
+    });
 
 const findByEmail = email =>
   models.users.findOne({ where: { email } }).catch(error => {
