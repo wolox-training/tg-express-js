b38ff5b36ea62381250aa2723b25601832e33cd2
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-08 15:43:18 -0300

    Moved validations to helpers to be reused in sign in

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -1,6 +1,16 @@
 const bcrypt = require('bcrypt');
 const errors = require('../errors');
 
+const validatePassword = password => {
+  const alphanumeric_regex = /^[a-z0-9]+$/i;
+  return alphanumeric_regex.test(password) && password.length >= 8;
+};
+
+const validateEmail = email => {
+  const wolox_email_regex = /@wolox.com/;
+  return wolox_email_regex.test(email);
+};
+
 const hash = str => {
   const HASH_SALT = 10;
   return bcrypt.hash(str, HASH_SALT).catch(error => {
@@ -9,5 +19,7 @@ const hash = str => {
 };
 
 module.exports = {
-  hash
+  hash,
+  validatePassword,
+  validateEmail
 };
--- app/middlewares/sign_up.js
+++ app/middlewares/sign_up.js
@@ -1,28 +1,20 @@
 const errors = require('../errors');
-
-const validatePassword = password => {
-  const alphanumeric_regex = /^[a-z0-9]+$/i;
-  const isPasswordValid = alphanumeric_regex.test(password) && password.length >= 8;
-
-  if (!isPasswordValid) {
-    throw errors.invalidPasswordError(
-      'Password must be alphanumeric and have a length of 8 or more characters'
-    );
-  }
-};
-
-const validateEmail = email => {
-  const wolox_email_regex = /@wolox.com/;
-  const isEmailValid = wolox_email_regex.test(email);
-
-  if (!isEmailValid) {
-    throw errors.invalidEmailError('Invalid email');
-  }
-};
+const { validatePassword, validateEmail } = require('../helpers/utils');
 
 module.exports = (req, res, next) => {
   const { email, password } = req.body.user;
-  validateEmail(email);
-  validatePassword(password);
+  const isEmailValid = validateEmail(email);
+  const isPasswordValid = validatePassword(password);
+
+  if (!isEmailValid) {
+    next(errors.invalidEmailError('Invalid email'));
+  }
+
+  if (!isPasswordValid) {
+    next(
+      errors.invalidPasswordError('Password must be alphanumeric and have a length of 8 or more characters')
+    );
+  }
+
   next();
 };
