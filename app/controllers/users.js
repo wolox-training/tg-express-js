db5a8b5bc714deeed78e82856e09bd3938006a7a
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-11 14:34:10 -0300

    Added sign in to users controller

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,6 +2,7 @@ const usersService = require('../services/users');
 
 const userSignUpRequest = require('../serializers/user_sign_up_request');
 const userSignUpResponse = require('../serializers/user_sign_up_response');
+const userSignInRequest = require('../serializers/user_sign_in_request');
 
 const signUp = (req, res, next) => {
   const { user } = req.body;
@@ -11,6 +12,15 @@ const signUp = (req, res, next) => {
     .catch(next);
 };
 
+const signIn = (req, res, next) => {
+  const { user } = req.body;
+  return usersService
+    .signIn(userSignInRequest(user))
+    .then(response => res.send(response))
+    .catch(next);
+};
+
 module.exports = {
-  signUp
+  signUp,
+  signIn
 };
