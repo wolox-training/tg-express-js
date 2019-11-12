27bcd13d1646656614f405935f836dd92b2bd37f
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-12 16:27:45 -0300

    Removed unnecessary validations

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -1,16 +1,6 @@
 const bcrypt = require('bcrypt');
 const errors = require('../errors');
 
-const validatePassword = password => {
-  const alphanumeric_regex = /^[a-z0-9]+$/i;
-  return alphanumeric_regex.test(password) && password.length >= 8;
-};
-
-const validateEmail = email => {
-  const wolox_email_regex = /@wolox.com/;
-  return wolox_email_regex.test(email);
-};
-
 const hash = str => {
   const HASH_SALT = 10;
   return bcrypt.hash(str, HASH_SALT).catch(error => {
@@ -22,7 +12,5 @@ const comparePassword = bcrypt.compare;
 
 module.exports = {
   hash,
-  validatePassword,
-  comparePassword,
-  validateEmail
+  comparePassword
 };
