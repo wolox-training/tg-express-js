d908854b502a190bbe9f648c788684b0d25c73e7
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-13 17:16:49 -0300

    Added sign in log, fixed error message typo

--- app/interactors/sign_in.js
+++ app/interactors/sign_in.js
@@ -1,17 +1,19 @@
 const usersService = require('../services/users');
 const errors = require('../errors');
 const { comparePassword, createToken } = require('../helpers/utils');
+const logger = require('../logger');
 
 module.exports = user =>
   usersService.findByEmail(user.email).then(foundUser => {
     if (!foundUser) {
-      throw errors.userDoesNotExistError('User already exists');
+      throw errors.userDoesNotExistError('User does not exist');
     }
 
     return comparePassword(user.password, foundUser.password).then(passwordMatches => {
       if (!passwordMatches) {
         throw errors.incorrectPasswordError('Incorrect password');
       }
+      logger.info(`User signed in: ${user.email}`);
       return createToken(user.email);
     });
   });
