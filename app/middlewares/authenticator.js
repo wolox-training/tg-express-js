07b08c60b7026cf60473ac50223c6df9d0ef22bb
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-13 15:06:15 -0300

    Added authenticator

--- app/middlewares/authenticator.js
+++ app/middlewares/authenticator.js
@@ -1,3 +1,8 @@
+const jwt = require('jsonwebtoken');
+const config = require('../../config');
+
 module.exports = (req, res, next) => {
+  const token = req.headers.authorization.slice(7);
+  jwt.verify(token, config.common.session.secret);
   next();
 };
