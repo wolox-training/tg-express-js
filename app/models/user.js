0d4be429ad8bd893df8637971a1294b8e9285da8
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-14 14:04:32 -0300

    Added migrations and changes to model for having admin users

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,8 @@ module.exports = (sequelize, DataTypes) => {
       firstName: { type: DataTypes.STRING, field: 'first_name', allowNull: false },
       lastName: { type: DataTypes.STRING, field: 'last_name', allowNull: false },
       email: { type: DataTypes.STRING, field: 'email', allowNull: false, unique: true },
-      password: { type: DataTypes.STRING, field: 'password', allowNull: false }
+      password: { type: DataTypes.STRING, field: 'password', allowNull: false },
+      isAdmin: { type: DataTypes.BOOLEAN, field: 'is_admin', defaultValue: false, allowNull: false }
     },
     {
       underscored: true,
