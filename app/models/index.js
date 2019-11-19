28ca3fcdefe8ff954bfb67da47adef7679b17a03
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-19 12:36:48 -0300

    Added conditional initialization of sequelize

--- app/models/index.js
+++ app/models/index.js
@@ -6,7 +6,21 @@ const dbConfig = require('../../config/db')[config.environment];
 
 const basename = path.basename(__filename);
 const db = {};
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+
+const initializeSequelize = () => {
+  if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    return new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      logging: true
+    });
+  }
+
+  return new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+};
+
+const sequelize = initializeSequelize();
 
 fs.readdirSync(__dirname)
   .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
