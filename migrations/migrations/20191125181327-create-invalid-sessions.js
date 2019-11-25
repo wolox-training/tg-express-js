f95ef3aaab3ce8cdd8e0e129a88d92d83b5cb235
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-25 16:23:12 -0300

    Created invalid sessions model

---
+++ migrations/migrations/20191125181327-create-invalid-sessions.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('invalid_sessions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+  down: queryInterface => queryInterface.dropTable('invalid_sessions')
+};
