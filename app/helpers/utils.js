6b6f6bd87b2c22b03921b1b5cdd9c0c194a1bada
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-26 11:26:53 -0300

    Added logging with expiration date for token

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const errors = require('../errors');
 const config = require('../../config');
 const { DEFAULT_PAGE, DEFAULT_LIMIT } = require('./constants');
+const logger = require('../logger');
 
 const hash = str => {
   const HASH_SALT = 10;
@@ -15,6 +16,12 @@ const comparePassword = bcrypt.compare;
 
 const createToken = payload => {
   const token = jwt.sign({ payload }, config.common.session.secret, { expiresIn: '24h' });
+
+  jwt.verify(token, config.common.session.secret, (err, decode) => {
+    const expirationDate = new Date(decode.exp * 1000);
+    logger.info(`Created token will expire at ${expirationDate}`);
+  });
+
   return {
     success: true,
     message: 'Authentication successful',
