84bbcdcf5114d30d03f2fdaa045982ce69211aef
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-26 17:41:16 -0300

    Added mailer config through environment variables

--- config/index.js
+++ config/index.js
@@ -44,6 +44,14 @@ const config = {
       apiDate: process.env.API_DATE || 'X-API-Date',
       packageVersion: process.env.PACKAGE_VERSION || 'X-Package-Version',
       nodeVersion: process.env.NODE_VERSION || 'X-Node-Version'
+    },
+    mailer: {
+      host: process.env.MAILER_HOST,
+      port: process.env.MAILER_PORT,
+      auth: {
+        user: process.env.MAILER_USER,
+        pass: process.env.MAILER_PASS
+      }
     }
   }
 };
